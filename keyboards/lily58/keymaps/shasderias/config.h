4e676ff8b64de30cdd4331bd9436adec782e5976
Lim Yue Chuan <s@shasderias.com>
2023-11-15 04:56:26 +0800

    shasderias-lily58

---
+++ keyboards/lily58/keymaps/shasderias/config.h
@@ -0,0 +1,40 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define EE_HANDS
+
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+
+#undef RGBLIGHT_LED_COUNT
+#undef WS2812_DI_PIN
+#define RGBLIGHT_LED_COUNT 2
+#define WS2812_DI_PIN 17
+#define RGBLED_SPLIT {1, 1}
+#define RGBLIGHT_LIMIT_VAL 255
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
---
+++ keyboards/lily58/keymaps/shasderias/rules.mk
@@ -0,0 +1,17 @@
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = no        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+SWAP_HANDS_ENABLE = no      # Enable one-hand typing
+OLED_ENABLE = no            # OLED display
+LEADER_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+BOOTLOADER = rp2040
+CONVERT_TO = kb2040
+CAPS_WORD_ENABLE = yes
+DEFERRED_EXEC_ENABLE = yes
