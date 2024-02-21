cf1c5f3e609d8639534a773d8dd6d95b82a738a4
Lim Yue Chuan <s@shasderias.com>
2024-02-21 12:37:20 +0800

    more stuff!

--- keyboards/lily58/keymaps/shasderias/config.h
+++ keyboards/lily58/keymaps/shasderias/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 
 #define QUICK_TAP_TERM 0
-#define TAPPING_TERM 160
+#define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define HOLD_ON_OTHER_KEY_PRESS
 
